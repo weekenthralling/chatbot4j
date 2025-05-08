d83d76c0311fe9c2927ef6f4699db4afba889c29
Mo Zhou <zc@zjuici.com>
2025-05-08 16:46:21 +0800

    feat: add huggingface embedding model

---
+++ src/main/java/dev/chatbot/aiservice/properties/EmbedProperties.java
@@ -0,0 +1,20 @@
+package dev.chatbot.aiservice.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+/**
+ * EmbedProperties is a configuration class that holds properties related to the embedding model.
+ * It is annotated with @ConfigurationProperties to bind properties from the application configuration file.
+ * The prefix "chatbot.embed" indicates that these properties are under the "chatbot.embed" section in the configuration file.
+ * 
+ * @author zhoumo
+ */
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "chatbot.embedding")
+public class EmbedProperties {
+    private String baseUrl;
+}
---
+++ src/main/java/dev/chatbot/utils/Json.java
@@ -0,0 +1,83 @@
+package dev.chatbot.utils;
+
+import java.text.SimpleDateFormat;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import dev.chatbot.exception.BusinessException;
+
+public class Json {
+
+    private Json() {
+        // Prevent instantiation
+    }
+    
+    private static final ObjectMapper mapper = new ObjectMapper();
+    private static final String DATE_TIME_FORMATTER = "yyyy-MM-dd HH:mm:ss";
+
+    /*
+     * Initialize configuration
+     */
+    static {
+        // All fields of the object are listed in
+        mapper.setSerializationInclusion(JsonInclude.Include.ALWAYS);
+        // Cancel the default conversion timestamps form
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        // Ignore the error of empty bean to json
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        // All date formats are uniformly in the following style, i.e. yyyy-MM-dd HH:mm:ss
+        mapper.setDateFormat(new SimpleDateFormat(DATE_TIME_FORMATTER));
+        // Ignore the case where there is a json string, but no corresponding attribute exists in the java object. Prevent errors
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+
+    /**
+     * Function description: Convert java objects into JSON data
+     *
+     *@param object java object
+     *@return JSON data
+     */
+    public static String toJson(Object object) {
+        try {
+            return mapper.writeValueAsString(object);
+        } catch (Exception e) {
+            throw new BusinessException("read from json failed");
+        }
+    }
+
+    /**
+     * Function description: Convert JSON data into java objects
+     *
+     *@param json JSON data
+     *@param clazz java object class
+     *@return java object
+     */
+    public static <T> T fromJson(String json, Class<T> clazz) {
+        try {
+            return mapper.readValue(json, clazz);
+        } catch (Exception e) {
+            throw new BusinessException("read from json failed");
+        }
+    }
+
+    /**
+     * Data conversion
+     *
+     * @param json          JSON data
+     * @param typeReference Corresponding type
+     * @param <T>           <T>
+     * @return <T>
+     */
+    public static <T> T fromJson(String json, TypeReference<T> typeReference) {
+        try {
+            return mapper.readValue(json, typeReference);
+        } catch (Exception e) {
+            throw new BusinessException("read from json with TypeReference failed");
+        }
+    }
+}
