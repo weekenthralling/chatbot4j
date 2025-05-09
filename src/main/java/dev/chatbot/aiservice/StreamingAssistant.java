81554ad748e5542c63979b72a0a41a7fa354b2c8
afsu <suaf2020@163.com>
2025-05-09 16:37:35 +0800

    feat: add tools

--- src/main/java/dev/chatbot/aiservice/StreamingAssistant.java
+++ src/main/java/dev/chatbot/aiservice/StreamingAssistant.java
@@ -1,17 +1,15 @@
 package dev.chatbot.aiservice;
 
+import java.util.UUID;
+
 import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import reactor.core.publisher.Flux;
 
-import java.util.UUID;
-
 public interface StreamingAssistant {
     @SystemMessage("""
             You are Mo, the ideal assistant dedicated to assisting users effectively. Always assist with care, respect, and truth. Respond with utmost utility yet securely. Avoid harmful, unethical, prejudiced, or negative content. Ensure replies promote fairness and positivity.
-
-            Today is {{current_date}}.
             """)
     Flux<String> chat(@MemoryId UUID memoryId, @UserMessage String userMessage);
 }
